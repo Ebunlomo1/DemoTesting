c719d2f23e093986660c5fba416e97d891afb490
Ebunlomo1 <ebunlomoadekunle@gmail.com>
2024-12-14 14:15:32 +0100

    Testing Git

---
+++ TestUsingTestNG/src/test/java/TestDemo.java
@@ -0,0 +1,40 @@
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	
+	
+	public void login() {
+
+		  
+
+		  System.out.println("Login to application....");
+
+		 }
+
+		 
+
+		 @Test(priority=3)
+
+		 public void logout() {
+
+		  
+
+		  System.out.println("logout from application....");
+
+		 }
+
+		 
+
+		 
+
+		 @Test(priority=-1)
+
+		 public void openapp() {
+
+		  
+
+		  System.out.println("opening application....");
+
+		 }
+
+}
